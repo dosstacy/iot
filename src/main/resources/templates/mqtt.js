6608f5dd70414b0375c89c7d72161d8806ceaa3f
Anastasiia Dosyn <anastasiia.dosyn@student.tuke.sk>
2025-01-05 16:37:51 +0100

    refactor: update mqtt js file

--- src/main/resources/templates/mqtt.js
+++ src/main/resources/templates/mqtt.js
@@ -1,37 +1,36 @@
-window.onload = function () {
-    const broker = "ws://147.232.205.176:1884/mqtt"; // WebSocket брокер
-    const topic = "services/alert/ad408gh/status";
+import Paho from 'paho-mqtt';
 
-// Підключення до брокера
-    const client = new Paho.MQTT.Client(broker, "web_client_" + Math.random().toString(16).substr(2, 8));
+const broker = "ws://147.232.205.176:1884/mqtt";
+const topic = "kpi/solaris/temperature/ad408gh";
 
-    client.onMessageArrived = function (message) {
-        const data = JSON.parse(message.payloadString); // Отримуємо JSON
-        console.log("Received data:", data);
+const client = new Paho.MQTT.Client(broker, "web_client_" + Math.random().toString(16).substr(2, 8));
 
-        // Оновлення даних на сторінці
-        if (data.temperature) {
-            document.querySelector(".temp-stats").textContent = `${data.temperature} °C`;
-        }
-        if (data.airHumidity) {
-            document.querySelector(".air-stats").textContent = `${data.airHumidity}%`;
-        }
-        if (data.soilHumidity) {
-            document.querySelector(".soil-stats").textContent = `${data.soilHumidity}%`;
-        }
-        if (data.lightLevel) {
-            document.querySelector(".light-stats").textContent = `${data.lightLevel}%`;
-            document.querySelector(".slider").value = data.lightLevel; // Синхронізація слайдера
-        }
-    };
+client.onMessageArrived = function (message) {
+    const data = JSON.parse(message.payloadString);
+    console.log("Received data:", data);
 
-    client.connect({
-        onSuccess: function () {
-            console.log("Connected to broker");
-            client.subscribe(topic);
-        },
-        onFailure: function (error) {
-            console.error("Connection failed:", error);
-        }
-    });
-}
+    // Оновлення даних на сторінці
+    if (data.temperature) {
+        document.querySelector(".temp-stats").textContent = `${data.temperature} °C`;
+    }
+    if (data.airHumidity) {
+        document.querySelector(".air-stats").textContent = `${data.airHumidity}%`;
+    }
+    if (data.soilHumidity) {
+        document.querySelector(".soil-stats").textContent = `${data.soilHumidity}%`;
+    }
+    if (data.lightLevel) {
+        document.querySelector(".light-stats").textContent = `${data.lightLevel}%`;
+        document.querySelector(".slider").value = data.lightLevel; // Синхронізація слайдера
+    }
+};
+
+client.connect({
+    onSuccess: function () {
+        console.log("Connected to broker");
+        client.subscribe(topic);
+    },
+    onFailure: function (error) {
+        console.error("Connection failed:", error);
+    }
+});
