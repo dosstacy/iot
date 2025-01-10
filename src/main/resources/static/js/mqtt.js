fc3b925030b8de92f9e86728c90b3aaa97c3d452
Anastasiia Dosyn <anastasiia.dosyn@student.tuke.sk>
2025-01-10 21:31:29 +0100

    refactor: separate logic to functions

--- src/main/resources/static/js/mqtt.js
+++ src/main/resources/static/js/mqtt.js
@@ -1,55 +1,63 @@
-const client = mqtt.connect('ws://147.232.205.176:8000', {
-    username: 'maker',
-    password: 'mother.mqtt.password'
-});
+function connectToMother(topics){
+    const client = mqtt.connect('ws://147.232.205.176:8000', {
+        username: 'maker',
+        password: 'mother.mqtt.password'
+    });
 
-const topics = {
-    temperature: 'kpi/solaris/temperature/ad408gh',
-    airHumidity: 'kpi/solaris/humidity/air/ad408gh',
-    soilHumidity: 'kpi/solaris/humidity/soil/ad408gh',
-    light: 'kpi/solaris/light/ad408gh'
-};
+    client.on('connect', () => {
+        console.log('Connected to MQTT broker');
 
-client.on('connect', () => {
-    console.log('Connected to MQTT broker');
-
-    Object.values(topics).forEach(topic => {
-        client.subscribe(topic, (err) => {
-            if (!err) {
-                console.log(`Subscribed to ${topic}`);
-            }
+        Object.values(topics).forEach(topic => {
+            client.subscribe(topic, (err) => {
+                if (!err) {
+                    console.log(`Subscribed to ${topic}`);
+                }
+            });
         });
     });
-});
 
-client.on('message', (topic, message) => {
-    console.log(`Received a message from topic ${topic}: ${message.toString()}`);
-    const data = JSON.parse(message.toString());
+    return client;
+}
 
-    switch(topic) {
-        case topics.temperature:
-            document.querySelector(".temp-stats").textContent = `${data.metrics[0]?.value} °C`;
-            break;
-        case topics.airHumidity:
-            document.querySelector(".air-stats").textContent = `${data.metrics[0]?.value} %`;
-            break;
-        case topics.soilHumidity:
-            document.querySelector(".soil-stats").textContent = `${data.metrics[0]?.value} %`;
-            break;
-        case topics.light:
-            document.querySelector(".light-stats").textContent = `${data.metrics[0]?.value} %`;
-            document.querySelector(".slider").value = data.metrics[0]?.value;
-            break;
-    }
-});
+function getAllDataFromMother() {
+    const topics = {
+        temperature: 'kpi/solaris/temperature/ad408gh',
+        airHumidity: 'kpi/solaris/humidity/air/ad408gh',
+        soilHumidity: 'kpi/solaris/humidity/soil/ad408gh',
+        light: 'kpi/solaris/light/ad408gh'
+    };
 
-client.on('error', (err) => {
-    console.error('Connection error:', err);
-});
+    let client = connectToMother(topics);
 
-client.on('close', () => {
-    console.log('Connection closed');
-});
+    client.on('message', (topic, message) => {
+        console.log(`Received a message from topic ${topic}: ${message.toString()}`);
+        const data = JSON.parse(message.toString());
+
+        switch (topic) {
+            case topics.temperature:
+                document.querySelector(".temp-stats").textContent = `${data.metrics[0]?.value} °C`;
+                break;
+            case topics.airHumidity:
+                document.querySelector(".air-stats").textContent = `${data.metrics[0]?.value} %`;
+                break;
+            case topics.soilHumidity:
+                document.querySelector(".soil-stats").textContent = `${data.metrics[0]?.value} %`;
+                break;
+            case topics.light:
+                document.querySelector(".light-stats").textContent = `${data.metrics[0]?.value} %`;
+                document.querySelector(".slider").value = data.metrics[0]?.value;
+                break;
+        }
+    });
+
+    client.on('error', (err) => {
+        console.error('Connection error:', err);
+    });
+
+    client.on('close', () => {
+        console.log('Connection closed');
+    });
+}
 
 
 
